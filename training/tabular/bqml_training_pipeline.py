68886d223cd1ea5958eb443e873344d0fb890977
ivanmkc <ivans.mailbox@gmail.com>
2021-08-11 10:49:58 -0400

    Added initial components for BQ

---
+++ training/tabular/bqml_training_pipeline.py
@@ -0,0 +1,50 @@
+# Copyright 2021 The Kubeflow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Dict, List
+
+import kfp
+from kfp.components import OutputPath
+from kfp.v2.dsl import Output, Dataset, component
+from pipelines_folder.pipeline import Pipeline
+from typing import Any, Callable, Optional
+from google_cloud_pipeline_components import aiplatform as gcc_aip
+from components.bigquery import bigquery
+
+
+class BQMLTrainingPipeline(Pipeline):
+    """
+    Runs a BQ query, creates a model and generates evaluations
+    """
+
+    def __init__(self, name: str):
+        super().__init__(name=name)
+
+    def create_pipeline(self, project: str, pipeline_root: str) -> Callable[..., Any]:
+        @kfp.dsl.pipeline(name=self.name, pipeline_root=pipeline_root)
+        def pipeline():
+            select_query = "TODO"
+            create_model_op = bigquery.create_model(
+                create_mode=bigquery.BQMLModelCreateMode.CREATE_MODEL.value,
+                model_name="test",
+                create_model_options="bigquery.BQMLCreateModelOptions()",
+                select_query=select_query,
+            )
+
+            roc_curve_op = bigquery.create_roc_curve(
+                model_name=create_model_op.output,
+                table_name="",
+                thresholds_str="",
+            )
+
+        return pipeline
---
+++ workspace/run_bq_pipeline.py
@@ -0,0 +1,98 @@
+from workspace.pipelines import pipelines
+
+from google.cloud import aiplatform
+from pipelines_folder.pipeline import Pipeline
+from kfp.v2 import compiler  # noqa: F811
+from kfp.v2.google.client import AIPlatformClient  # noqa: F811
+from typing import List, Set
+
+from training.tabular.bqml_training_pipeline import BQMLTrainingPipeline
+
+
+def check_if_dataset_changed(
+    dataset_id: str,
+) -> bool:
+    # TODO: Check if dataset_id was already trained
+    # Perhaps modify the dataset metadata somehow
+    return True
+
+
+def get_changed_datasets() -> Set[str]:
+    # TODO: Get all datasets
+
+    return []
+
+
+def retrain_for_changed_datasets(project_id: str, region: str):
+    # Get last modified dates
+    datasets = aiplatform.Dataset.list()
+
+    # Get all datasets
+    changed_datasets = [
+        dataset
+        for dataset in datasets
+        if check_if_dataset_changed(dataset_id=dataset.id)
+    ]
+
+    # Get all pipelines for changed datasets
+    all_pipelines: List[Pipeline] = []  # TODO
+
+    changed_dataset_uris = set([changed_datasets.uri for dataset in changed_datasets])
+
+    # Run all pipelines that use a modified dataseta
+    for pipeline in all_pipelines:
+        if pipeline.managed_dataset_uri in changed_dataset_uris:
+            run_pipeline(
+                project_id=project_id,
+                region=region,
+                pipeline_root=pipeline_root,
+                pipeline=pipeline,
+            )
+
+
+JOB_SPEC_PATH = "package.json"
+
+
+def run_pipeline(
+    project_id: str,
+    region: str,
+    pipeline_root: str,
+    pipeline: Pipeline,
+):
+    compiler.Compiler().compile(
+        pipeline_func=pipeline.create_pipeline(
+            project=project_id, pipeline_root=pipeline_root
+        ),
+        package_path=JOB_SPEC_PATH,
+    )
+
+    api_client = AIPlatformClient(project_id=project_id, region=region)
+
+    # TODO: Replace with something that blocks and throws an error
+    response = api_client.create_run_from_job_spec(
+        JOB_SPEC_PATH,
+        pipeline_root=pipeline_root,
+        parameter_values={},
+    )
+
+
+BUCKET_NAME = "gs://ivanmkc-test2/pipeline_staging"
+pipeline_root = "{}/pipeline_root".format(BUCKET_NAME)
+
+# Perform a query.
+query = (
+    "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
+    'WHERE state = "TX" '
+    "LIMIT 100"
+)
+
+pipeline = BQMLTrainingPipeline(name="my-model-name")
+
+for pipeline in [pipeline]:
+    print(f"Running pipeline: {pipeline.name}")
+    run_pipeline(
+        project_id="python-docs-samples-tests",
+        region="us-central1",
+        pipeline_root=pipeline_root,
+        pipeline=pipeline,
+    )
